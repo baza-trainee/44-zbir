aeb449ee86f1073838e7c88fd50325582aa07e19
artyomakulov <artyomakulov@gmail.com>
2024-04-02 15:55:02 +0300

    fix classname

--- src/components/DonationBtn/DonationBtn.jsx
+++ src/components/DonationBtn/DonationBtn.jsx
@@ -3,7 +3,7 @@ import styles from "./DonationBtn.module.css";
 const DonationBtn = () => {
   return (
     <a href="/" target="_blank" rel="noopener noreferrer">
-      <button className={styles.helpLink}>Допомогти</button>
+      <button className={styles.donationBtn}>Допомогти</button>
     </a>
   );
 };
