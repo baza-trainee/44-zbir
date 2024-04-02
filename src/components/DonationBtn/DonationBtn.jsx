0e5042d96b8156d0f3d21513540a1579ad50b01e
artyomakulov <artyomakulov@gmail.com>
2024-04-02 15:49:40 +0300

    fix active

--- src/components/DonationBtn/DonationBtn.jsx
+++ src/components/DonationBtn/DonationBtn.jsx
@@ -1,28 +1,9 @@
 import styles from "./DonationBtn.module.css";
 
-import { useState } from "react";
-
 const DonationBtn = () => {
-  const [isPressed, setIsPressed] = useState(false);
-
-  const handleLinkClick = () => {
-    setIsPressed(true);
-  };
-
-  const handleLinkRelease = () => {
-    setIsPressed(false);
-  };
-
   return (
     <a href="/" target="_blank" rel="noopener noreferrer">
-      <button
-        className={`${styles.helpLink} ${isPressed ? styles.active : ""}`}
-        onMouseDown={handleLinkClick}
-        onMouseUp={handleLinkRelease}
-        onMouseLeave={handleLinkRelease}
-      >
-        Допомогти
-      </button>
+      <button className={styles.helpLink}>Допомогти</button>
     </a>
   );
 };
--- src/components/ShareButton/ShareButton.jsx
+++ src/components/ShareButton/ShareButton.jsx
@@ -1,24 +1,9 @@
-import React, { useState } from "react";
 import { ReactComponent as ShareImage } from "./share-nodes-outline.svg";
 import styles from "./ShareButton.module.css";
 
 const ShareButton = () => {
-  const [isPressed, setIsPressed] = useState(false);
-
-  const handleLinkClick = () => {
-    setIsPressed(true);
-  };
-
-  const handleLinkRelease = () => {
-    setIsPressed(false);
-  };
   return (
-    <button
-      className={`${styles.shareButton} ${isPressed ? styles.active : ""}`}
-      onMouseDown={handleLinkClick}
-      onMouseUp={handleLinkRelease}
-      onMouseLeave={handleLinkRelease}
-    >
+    <button className={styles.shareButton}>
       Поділитися
       <ShareImage className={styles.svg} />
     </button>
