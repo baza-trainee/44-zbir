7fcdc2e321f6f020fa019ea38663afacd5209033
bohdanalu <b_dana@ukr.net>
2024-04-03 08:36:38 +0300

    feat: add section Important

--- src/components/Important/Important.jsx
+++ src/components/Important/Important.jsx
@@ -1,11 +1,29 @@
-//import css from './Important.module.css'
+import css from "./Important.module.css";
+
+const importantList = [
+  { title: "Авто - це швидкий та мобільний вихід на позиції" },
+  { title: "Авто - це потужний та надійний засіб доставки зброї" },
+  { title: "Авто - це бойовий побратим, який допомагає рятувати життя" },
+];
 
 const Important = () => {
-    return (
-        <>
-            <h2>Important section</h2>
-        </>
-    );
+  return (
+    <section className={css.section}>
+      <div className={css.container}>
+        <h2 className={css.title}>Чому це важливо</h2>
+        <ul className={css.list}>
+          {importantList.map((item, index) => {
+            return (
+              <li className={css.item} key={index}>
+                <img className={css.img} src="" alt="Фото авто" />
+                <p className={css.text}>{item.title}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Important;
