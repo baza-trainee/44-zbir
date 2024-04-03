40c405b907fa7a0d00ec0f7200de83771c25895b
artyomakulov <artyomakulov@gmail.com>
2024-04-03 12:38:24 +0300

    add onClick

--- src/components/ShareButton/ShareButton.jsx
+++ src/components/ShareButton/ShareButton.jsx
@@ -1,9 +1,9 @@
 import { ReactComponent as ShareImage } from "./share-nodes-outline.svg";
 import styles from "./ShareButton.module.css";
 
-const ShareButton = () => {
+const ShareButton = ({ onClick }) => {
   return (
-    <button className={styles.shareButton}>
+    <button className={styles.shareButton} onClick={() => console.log("click")}>
       Поділитися
       <ShareImage className={styles.svg} />
     </button>
