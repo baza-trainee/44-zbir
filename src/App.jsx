cde39c7e86f8e93d0e1371f70d0ad56da475baf6
artyomakulov <artyomakulov@gmail.com>
2024-04-03 11:53:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-import DonationBtn from "./components/DonationBtn/DonatiomBtn";
+import DonationBtn from "./components/DonationBtn/DonationBtn";
 import StatusBar from "./components/StatusBar/StatusBar";
 import About from "./components/About/About";
 import Important from "./components/Important/Important";
 import Reward from "./components/Reward/Reward";
 import Footer from "./components/Footer/Footer";
 import ReturnBtn from "./components/ReturnBtn/ReturnBtn";
+import ModalShare from "./components/ModalShare/ModalShare";
+import ShareButton from "./components/ShareButton/ShareButton";
 
 const App = () => {
   return (
@@ -20,6 +22,7 @@ const App = () => {
       <Reward />
       <Footer />
       <ReturnBtn />
+      <ModalShare />
       <ShareButton />
     </>
   );
