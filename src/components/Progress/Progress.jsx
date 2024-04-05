ac119fd4f8729ba305f1b9ca86e48165b227d45b
Brodich Vlad <brodich_vlad@ukr.net>
2024-04-05 18:19:19 +0300

    fix:fix component Progres

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -3,18 +3,26 @@ import progress_element from '../../images/status-bar/progress_element.png';
 
 
 const Progress = ({ balance }) => {
-function createElementsProgress(balance) {
+	function createElementsProgress(balance) {
+	
+	const w = document.documentElement.clientWidth;
+	let progress = 50;
+	if (w < 650) {
+		progress = Math.floor(w * 0.074);
+	}
+
 	let num = (balance / 1000).toFixed() < 1 ? 0 : (balance / 1000).toFixed();
 	if (num > 50) {
 		num = 50;
-	}
+		}
+		
 	const elements = [];
-	for (let i = 0; i <= 50; i++) {
+	for (let i = 0; i <= ((progress / 50) * num); i++) {
 		elements.push(<img className={css.progress_img} key={i * 15} src={progress_element} alt="progress element" />);
 	}
 	return elements;
 }
-
+console.log(document.documentElement.clientWidth);
   return <div className={css.progress}>{createElementsProgress(balance)}</div>;
 };
 export default Progress;
