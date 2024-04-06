b2ecca2f23a70e955da7868d76f06199f94809d1
shaman2507 <alabay174@gmail.com>
2024-04-06 17:50:54 +0200

    fix statusBar

--- src/components/StatusBar/StatusBar.jsx
+++ src/components/StatusBar/StatusBar.jsx
@@ -28,14 +28,6 @@ function createElementsIndicator(cost) {
 
 const StatusBar = () => {
 	const [cost, setCost] = useState(0);
-	// !!! Після вдалих тестів видалити.
-	function testInput(event) {
-		if (event.target.value >= 0) {
-			setCost(+event.target.value);
-		} else setCost(0);
-		if (event.target.value <= 60000) {
-			setCost(+event.target.value);
-		} else setCost(50000);
 	}
 
 	return (
@@ -53,11 +45,6 @@ const StatusBar = () => {
 			</div>
 			<div className={css.indicator}>{createElementsIndicator(cost)}</div>
 			<DonationBtn/>
-			{/* !!! Після вдалих тестів видалити. */}
-			<div className={css.test_bloc__section_status_bar}>
-				<label htmlFor="test">Тимчасовий інпут тест status bar</label>
-				<input onInput={testInput} type="number" id="test" min={0} max={60000} defaultValue={0} />
-			</div>
 		</section>
 	);
 };
