5edbcdea201c8d7631a04e6c085950f3b889c836
shaman2507 <alabay174@gmail.com>
2024-04-10 20:24:58 +0200

    update

--- src/constants/url_monobanc.js
+++
@@ -1,9 +0,0 @@
-// Посилання на монобанку.
-export const DONATION_URL = 'https://send.monobank.ua/jar/AC7zMvXYt3';
-// URL балансу до монобанки через JAR_ID.
-const BALANSE_BASE_URL = 'https://api.monobank.ua/bank/jar/';
-// JAR_ID нашої банки.
-const JAR_ID = '2TDxqw1vqQaxKFe74JT8jzmmu8Ra8ErB';
-
-export const BALANSE_URL = `${BALANSE_BASE_URL}${JAR_ID}`;
-// Залишились питання по налаштуванню - пишіть: brodich_vlad@ukr.net
--- src/helpers/separator.js
+++
@@ -1,9 +0,0 @@
-function thousandSeparator(num) {
-  const str = String(num.toFixed());
-	const reg = /\d{1,3}(?=(\d{3})+(?!\d))/g;
-	if (str === '') {
-		return '0';
-	} else return str.replace(reg, '$& ').replace(/\./g, ',');
-}
-
-export default thousandSeparator;
